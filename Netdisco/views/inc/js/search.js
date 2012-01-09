9e9d40885701cc6e38e490e1551f29a01a5794e2
Oliver Gorwits <oliver@cpan.org>
2012-01-09 10:58:28 +0000

    highlight active search filters in green

--- Netdisco/views/inc/js/search.js
+++ Netdisco/views/inc/js/search.js
@@ -72,4 +72,36 @@
       $(this).parents('.add-on').toggleClass('active', $(this).is(':checked'));
     };
     $('.add-on :checkbox').each(syncCheckBox).click(syncCheckBox);
+
+    // highlight active search filters in green
+    // there must be a way to factor this out to a func but my JS is weak :-/
+
+    $("form .clearfix input").not("[name=q]").each(function() {
+      if ($(this).val() === "") {
+        $(this).parent(".clearfix").removeClass('success');
+      } else {
+        $(this).parent(".clearfix").addClass('success');
+      }
+    });
+    $("form .clearfix input").not("[name=q]").change(function() {
+      if ($(this).val() === "") {
+        $(this).parent(".clearfix").removeClass('success');
+      } else {
+        $(this).parent(".clearfix").addClass('success');
+      }
+    });
+    $("form .clearfix select").each(function() {
+      if ($(this).find(":selected").length === 0) {
+        $(this).parent(".clearfix").removeClass('success');
+      } else {
+        $(this).parent(".clearfix").addClass('success');
+      }
+    });
+    $("form .clearfix select").change(function() {
+      if ($(this).find(":selected").length === 0) {
+        $(this).parent(".clearfix").removeClass('success');
+      } else {
+        $(this).parent(".clearfix").addClass('success');
+      }
+    });
   });
