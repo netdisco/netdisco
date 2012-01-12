659b984fec4fc680f0976d129ca29a91014a87b9
Oliver Gorwits <oliver@cpan.org>
2012-01-12 21:19:32 +0000

    implement vlan hide/show when list is larger than 10

--- Netdisco/views/inc/js/device.js
+++ Netdisco/views/inc/js/device.js
@@ -66,6 +66,15 @@
           }
           // looks good, update the bookmark for this search
           $(mark).attr('href', '/device?' + query);
+
+          // enable collapser on any large vlan lists
+          $('.nd_collapse_vlans').collapser({
+            target: 'siblings',
+            effect: 'slide',
+            changeText: true,
+            expandHtml: '<div class="nd_collapser">Show VLANs<div class="cell-arrow-up"></div></div>',
+            collapseHtml: '<div class="nd_collapser">Hide VLANs<div class="cell-arrow-down"></div></div>',
+          });
         }
       );
     }
