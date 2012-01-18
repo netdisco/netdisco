59016ee6afacb0e7f95856166ec5c36af1fd4b62
Oliver Gorwits <oliver@cpan.org>
2012-01-18 14:57:22 +0000

    make authN hook more liberal in path check

--- Netdisco/lib/Netdisco/Web/AuthN.pm
+++ Netdisco/lib/Netdisco/Web/AuthN.pm
@@ -6,7 +6,7 @@ use Dancer::Plugin::DBIC;
 use Digest::MD5 ();
 
 hook 'before' => sub {
-    if (! session('user') && request->path !~ m{^/login}) {
+    if (! session('user') && request->path !~ m{/login$}) {
         if (setting('environment') eq 'development' and setting('no_auth')) {
             session(user => 'developer');
         }
