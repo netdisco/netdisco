f574cd1db56abc0ba2fcf890dd19951fae7b0b76
Oliver Gorwits <oliver@cpan.org>
2012-01-20 23:47:25 +0000

    implement sidebar hiding for device view

--- Netdisco/views/js/device.js
+++ Netdisco/views/js/device.js
@@ -113,4 +113,24 @@
       $(this).parents('.add-on').toggleClass('active', $(this).is(':checked'));
     };
     $('.add-on :checkbox').each(syncCheckBox).click(syncCheckBox);
+
+    // sidebar toggle
+    $('#sidebar_toggle_img_in').click(
+      function() {
+        $('.sidebar').toggle(
+          function() {
+            $('#sidebar_toggle_img_out').toggle();
+            $('.nd_content').animate({'margin-left': '5px !important'}, 100);
+          }
+        );
+      }
+    );
+    $('#sidebar_toggle_img_out').click(
+      function() {
+        $('#sidebar_toggle_img_out').toggle();
+        $('.nd_content').animate({'margin-left': '225px !important'}, 200,
+          function() { $('.sidebar').toggle(200) }
+        );
+      }
+    );
   });
