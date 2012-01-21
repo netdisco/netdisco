00561ca43971fcfe369ae7351c3efc120b68e2d3
Oliver Gorwits <oliver@cpan.org>
2012-01-21 01:28:27 +0000

    fix the sidebar

--- Netdisco/views/js/sidebar.js
+++ Netdisco/views/js/sidebar.js
@@ -1,7 +1,6 @@
     // sidebar toggle
     $('#sidebar_toggle_img_in').click(
       function() {
-        $('#sidebar_toggle_img_in').toggle();
         $('.sidebar').toggle(
           function() {
             $('#sidebar_toggle_img_out').toggle();
@@ -16,7 +15,6 @@
         $('.nd_content').animate({'margin-left': '225px !important'}, 200,
           function() {
             $('.sidebar').toggle(200);
-            $('#sidebar_toggle_img_in').toggle();
           }
         );
       }
