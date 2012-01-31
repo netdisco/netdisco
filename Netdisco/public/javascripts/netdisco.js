c7bae9b6385e744f7c77a3f68b4d934bf590f8fb
Oliver Gorwits <oliver@cpan.org>
2012-01-31 16:59:55 +0000

    first impl. of RHS sidebar

--- Netdisco/public/javascripts/netdisco.js
+++ Netdisco/public/javascripts/netdisco.js
@@ -120,7 +120,7 @@ $(document).ready(function() {
       $('.sidebar').toggle(
         function() {
           $('#sidebar_toggle_img_out').toggle();
-          $('.nd_content').animate({'margin-left': '5px !important'}, 100);
+          $('.content').animate({'margin-right': '10px !important'}, 100);
           $('.device_label_right').toggle();
         }
       );
@@ -129,7 +129,7 @@ $(document).ready(function() {
   $('#sidebar_toggle_img_out').click(
     function() {
       $('#sidebar_toggle_img_out').toggle();
-      $('.nd_content').animate({'margin-left': '225px !important'}, 200,
+      $('.content').animate({'margin-right': '220px !important'}, 200,
         function() {
           $('.device_label_right').toggle();
           $('.sidebar').toggle(200);
