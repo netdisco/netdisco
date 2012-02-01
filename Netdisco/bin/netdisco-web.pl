cf1dd009ba538f9a3be4a8c23892dd96cba6d561
Oliver Gorwits <oliver@cpan.org>
2012-02-01 20:42:14 +0000

    revert changes to startup script

--- Netdisco/bin/netdisco-web.pl
+++ Netdisco/bin/netdisco-web.pl
@@ -1,9 +1,4 @@
 #!/usr/bin/env perl
-
-# use lib '/root/perl-profiles/netdisco-web/lib/perl5';
-use local::lib '/srv/www/vhosts/netdisco.ecmwf.int/perl-profiles/netdisco-web';
-use lib '/srv/www/vhosts/netdisco.ecmwf.int/Netdisco/lib';
-
 use Dancer;
 use Netdisco::Web;
 dance;
