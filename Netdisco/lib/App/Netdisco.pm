bfa65fc164857faa451138cddb32a9f8f1cf9dce
Oliver Gorwits <oliver@cpan.org>
2013-01-06 02:01:06 +0000

    first indexed release to cpan

--- Netdisco/lib/App/Netdisco.pm
+++ Netdisco/lib/App/Netdisco.pm
@@ -7,7 +7,7 @@ use 5.010_000;
 use File::ShareDir 'dist_dir';
 use Path::Class;
 
-our $VERSION = '2.001000_001';
+our $VERSION = '2.002000';
 
 BEGIN {
   if (not length ($ENV{DANCER_APPDIR} || '')
