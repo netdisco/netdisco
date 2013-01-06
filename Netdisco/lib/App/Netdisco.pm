b5f1ed7dbf66888f6f55f8c67be6716310d2ecd1
Oliver Gorwits <oliver@cpan.org>
2013-01-06 01:14:04 +0000

    bump version to test new scheme

--- Netdisco/lib/App/Netdisco.pm
+++ Netdisco/lib/App/Netdisco.pm
@@ -7,7 +7,7 @@ use 5.010_000;
 use File::ShareDir 'dist_dir';
 use Path::Class;
 
-our $VERSION = '2.01_001';
+our $VERSION = '2.001000_001';
 
 BEGIN {
   if (not length ($ENV{DANCER_APPDIR} || '')
