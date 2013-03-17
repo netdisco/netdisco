cbb1e8074a87bcbadd67285e482316d8f5d3fba1
Oliver Gorwits <oliver@cpan.org>
2013-03-17 14:48:23 +0000

    bump version for release

--- Netdisco/lib/App/Netdisco.pm
+++ Netdisco/lib/App/Netdisco.pm
@@ -7,7 +7,7 @@ use 5.010_000;
 use File::ShareDir 'dist_dir';
 use Path::Class;
 
-our $VERSION = '2.006000';
+our $VERSION = '2.007000_001';
 
 BEGIN {
   if (not length ($ENV{DANCER_APPDIR} || '')
