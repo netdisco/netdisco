09829a25b8d0a60d4c014ac6ebed35f0cf233a56
Oliver Gorwits <oliver@cpan.org>
2013-06-03 20:07:31 +0100

    local plugins site_plugins dir

--- Netdisco/lib/App/Netdisco/Web.pm
+++ Netdisco/lib/App/Netdisco/Web.pm
@@ -8,6 +8,7 @@ use Dancer::Plugin::DBIC;
 use Socket6 (); # to ensure dependency is met
 use HTML::Entities (); # to ensure dependency is met
 use URI::QueryParam (); # part of URI, to add helper methods
+use Path::Class 'dir';
 
 use App::Netdisco::Web::AuthN;
 use App::Netdisco::Web::Static;
@@ -38,6 +39,7 @@ if (setting('web_plugins') and ref [] eq ref setting('web_plugins')) {
 }
 
 if (setting('extra_web_plugins') and ref [] eq ref setting('extra_web_plugins')) {
+    unshift @INC, dir(($ENV{NETDISCO_HOME} || $ENV{HOME}), 'site_plugins')->stringify;
     _load_web_plugins( setting('extra_web_plugins') );
 }
 
