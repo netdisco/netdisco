5d4df72a244b1defc8963a420ba619229cf92fc1
Eric A. Miller <emiller@cpan.org>
2013-09-18 23:50:27 -0400

    add csv download to ap channel distribution report

--- Netdisco/lib/App/Netdisco/Web/Plugin/Report/ApChannelDist.pm
+++ Netdisco/lib/App/Netdisco/Web/Plugin/Report/ApChannelDist.pm
@@ -14,7 +14,7 @@ register_report(
     }
 );
 
-ajax '/ajax/content/report/apchanneldist' => require_login sub {
+sub get_rs_apchdist {
     my $set = schema('netdisco')->resultset('DevicePortWireless')->search(
         { channel => { '!=', '0' } },
         {   select   => [ 'channel', { count => 'channel' } ],
@@ -23,12 +23,36 @@ ajax '/ajax/content/report/apchanneldist' => require_login sub {
             order_by => { -desc => [qw/count/] },
         },
     );
+    return $set;
+}
+
+ajax '/ajax/content/report/apchanneldist' => require_login sub {
+    my $set = get_rs_apchdist();
 
     return unless $set->count;
 
     content_type('text/html');
     template 'ajax/report/apchanneldist.tt', { results => $set, },
         { layout => undef };
+};
+
+get '/ajax/content/report/apchanneldist' => require_login sub {
+    my $format = param('format');
+    my $set    = get_rs_apchdist();
+
+    return unless $set->count;
+
+    if ( $format eq 'csv' ) {
+
+        header( 'Content-Type' => 'text/comma-separated-values' );
+        header( 'Content-Disposition' =>
+                "attachment; filename=\"apchanneldist.csv\"" );
+        template 'ajax/report/apchanneldist_csv.tt', { results => $set, },
+            { layout => undef };
+    }
+    else {
+        return;
+    }
 };
 
 true;
