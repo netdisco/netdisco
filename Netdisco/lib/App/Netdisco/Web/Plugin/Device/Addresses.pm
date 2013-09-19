bb1f842d920c5960ea76e503a0696c6cd6691906
Eric A. Miller <emiller@cpan.org>
2013-09-18 23:35:28 -0400

    add csv download option to device addresses

--- Netdisco/lib/App/Netdisco/Web/Plugin/Device/Addresses.pm
+++ Netdisco/lib/App/Netdisco/Web/Plugin/Device/Addresses.pm
@@ -7,22 +7,46 @@ use Dancer::Plugin::Auth::Extensible;
 
 use App::Netdisco::Web::Plugin;
 
-register_device_tab({ tag => 'addresses', label => 'Addresses' });
+register_device_tab( { tag => 'addresses', label => 'Addresses' } );
 
 # device interface addresses
 ajax '/ajax/content/device/addresses' => require_login sub {
     my $q = param('q');
 
-    my $device = schema('netdisco')->resultset('Device')
-      ->search_for_device($q) or send_error('Bad device', 400);
+    my $device
+        = schema('netdisco')->resultset('Device')->search_for_device($q)
+        or send_error( 'Bad device', 400 );
 
-    my $set = $device->device_ips->search({}, {order_by => 'alias'});
+    my $set = $device->device_ips->search( {}, { order_by => 'alias' } );
     return unless $set->count;
 
     content_type('text/html');
-    template 'ajax/device/addresses.tt', {
-      results => $set,
-    }, { layout => undef };
+    template 'ajax/device/addresses.tt', { results => $set, },
+        { layout => undef };
+};
+
+get '/device/addresses' => require_login sub {
+    my $q      = param('q');
+    my $format = param('format');
+
+    my $device
+        = schema('netdisco')->resultset('Device')->search_for_device($q)
+        or send_error( 'Bad device', 400 );
+
+    my $set = $device->device_ips->search( {}, { order_by => 'alias' } );
+    return unless $set->count;
+
+    if ( $format eq 'csv' ) {
+
+        header( 'Content-Type' => 'text/comma-separated-values' );
+        header( 'Content-Disposition' =>
+                "attachment; filename=\"addresses.csv\"" );
+        template 'ajax/device/addresses_csv.tt', { results => $set, },
+            { layout => undef };
+    }
+    else {
+        return;
+    }
 };
 
 true;
