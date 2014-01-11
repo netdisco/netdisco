19823e13ffce0a25b79df42e61b95f1b33ad4785
Eric A. Miller <emiller@cpan.org>
2014-01-10 20:13:26 -0500

    [#64] Addition of Wireshark abbreviations for OUI manufacturers

--- Netdisco/lib/App/Netdisco/DB/Result/Oui.pm
+++ Netdisco/lib/App/Netdisco/DB/Result/Oui.pm
@@ -14,6 +14,8 @@ __PACKAGE__->add_columns(
   { data_type => "varchar", is_nullable => 0, size => 8 },
   "company",
   { data_type => "text", is_nullable => 1 },
+  "abbrev",
+  { data_type => "text", is_nullable => 1 },
 );
 __PACKAGE__->set_primary_key("oui");
 
---
+++ Netdisco/lib/App/Netdisco/DB/schema_versions/App-Netdisco-DB-32-33-PostgreSQL.sql
@@ -0,0 +1,6 @@
+
+BEGIN;
+
+ALTER TABLE oui ADD COLUMN abbrev text;
+
+COMMIT;
