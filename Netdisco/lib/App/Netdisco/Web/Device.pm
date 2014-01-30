132dc97bc639feacd25a00365b83293a824120ab
Oliver Gorwits <oliver@cpan.org>
2014-01-30 07:14:43 +0000

    on call to /device, check for duplicates by dns

--- Netdisco/lib/App/Netdisco/Web/Device.pm
+++ Netdisco/lib/App/Netdisco/Web/Device.pm
@@ -154,20 +154,29 @@ hook 'before_template' => sub {
 
 get '/device' => require_login sub {
     my $q = param('q');
-    my $dev = schema('netdisco')->resultset('Device')->single({
+    my $schema = schema('netdisco')->resultset('Device');
+
+    # we are passed either dns or ip
+    my $dev = $schema->search({
         -or => [
             \[ 'host(me.ip) = ?' => [ bind_value => $q ] ],
             'me.dns' => $q,
         ],
     });
 
-    if (!defined $dev) {
+    if ($dev->count == 0) {
         return redirect uri_for('/', {nosuchdevice => 1})->path_query;
     }
+
+    # if passed dns, need to check for duplicates
+    # and use only ip for q param, if there are duplicates.
+    my $first = $dev->first;
+    my $others = ($schema->search({dns => $first->dns})->count() - 1);
 
     params->{'tab'} ||= 'details';
     template 'device', {
       d => $dev,
+      display_name => ($others ? $first->ip : $first->dns),
       device => params->{'tab'},
     };
 };
