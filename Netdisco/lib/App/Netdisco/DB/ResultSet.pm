8e8839dd43f7724efbad7791b59d681cc5fc1796
Eric A. Miller <emiller@cpan.org>
2014-02-08 20:40:05 -0500

    Move get_distinct_col to App::Netdisco::DB::ResultSet so it can be used in all ResultSet's

--- Netdisco/lib/App/Netdisco/DB/ResultSet.pm
+++ Netdisco/lib/App/Netdisco/DB/ResultSet.pm
@@ -8,4 +8,25 @@ use base 'DBIx::Class::ResultSet';
 __PACKAGE__->load_components(
     qw{Helper::ResultSet::SetOperations Helper::ResultSet::Shortcut});
 
+=head2 get_distinct_col( $column )
+
+Returns an asciibetical sorted list of the distinct values in the given column
+of the Device table. This is useful for web forms when you want to provide a
+drop-down list of possible options.
+
+=cut
+
+sub get_distinct_col {
+  my ($rs, $col) = @_;
+  return $rs unless $col;
+
+  return $rs->search({},
+    {
+      columns => [$col],
+      order_by => $col,
+      distinct => 1
+    }
+  )->get_column($col)->all;
+}
+
 1;
--- Netdisco/lib/App/Netdisco/DB/ResultSet/Device.pm
+++ Netdisco/lib/App/Netdisco/DB/ResultSet/Device.pm
@@ -518,27 +518,6 @@ sub get_releases {
 
 }
 
-=head2 get_distinct_col( $column )
-
-Returns an asciibetical sorted list of the distinct values in the given column
-of the Device table. This is useful for web forms when you want to provide a
-drop-down list of possible options.
-
-=cut
-
-sub get_distinct_col {
-  my ($rs, $col) = @_;
-  return $rs unless $col;
-
-  return $rs->search({},
-    {
-      columns => [$col],
-      order_by => $col,
-      distinct => 1
-    }
-  )->get_column($col)->all;
-}
-
 =head2 with_port_count
 
 This is a modifier for any C<search()> which
