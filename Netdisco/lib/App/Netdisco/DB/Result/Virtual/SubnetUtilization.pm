4d94a8b4bed55981b7386e95e9d4ec2750f7e7b7
Eric A. Miller <emiller@cpan.org>
2014-02-25 22:32:26 -0500

    make virtual table name match file name in DB::Result::Virtual::SubnetUtilization

--- Netdisco/lib/App/Netdisco/DB/Result/Virtual/SubnetUtilization.pm
+++ Netdisco/lib/App/Netdisco/DB/Result/Virtual/SubnetUtilization.pm
@@ -8,7 +8,7 @@ use base 'DBIx::Class::Core';
 
 __PACKAGE__->table_class('DBIx::Class::ResultSource::View');
 
-__PACKAGE__->table('cidr_ips');
+__PACKAGE__->table('subnet_utilization');
 __PACKAGE__->result_source_instance->is_virtual(1);
 __PACKAGE__->result_source_instance->view_definition(<<'ENDSQL');
   SELECT net as subnet,
