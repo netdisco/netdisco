c9afbab26b80e85a373330c5c3f1a3297b7f6b00
Oliver Gorwits <oliver@cpan.org>
2014-05-10 21:36:01 +0100

    use Module::Load tricks to avoid some other mess

--- Netdisco/lib/App/Netdisco/Daemon/JobQueue.pm
+++ Netdisco/lib/App/Netdisco/Daemon/JobQueue.pm
@@ -5,16 +5,16 @@ use Dancer qw/:moose :syntax :script/;
 use Role::Tiny;
 use namespace::clean;
 
-with 'App::Netdisco::Daemon::JobQueue::'. setting('job_queue');
+use Module::Load ();
+Module::Load::load_remote 'JobQueue' =>
+  'App::Netdisco::JobQueue::' . setting('job_queue') => ':all';
 
-requires qw/
-  jq_get
-  jq_getlocal
-  jq_queued
-  jq_lock
-  jq_defer
-  jq_complete
-  jq_insert
-/;
+sub jq_get      { shift and JobQueue::jq_get(@_) }
+sub jq_getlocal { shift and JobQueue::jq_getlocal(@_) }
+sub jq_queued   { shift and JobQueue::jq_queued(@_) }
+sub jq_lock     { shift and JobQueue::jq_lock(@_) }
+sub jq_defer    { shift and JobQueue::jq_defer(@_) }
+sub jq_complete { shift and JobQueue::jq_complete(@_) }
+sub jq_insert   { shift and JobQueue::jq_insert(@_) }
 
 true;
