c12034d2b0d213f38bc7c74b86f2ddf4bbe0175e
Oliver Gorwits <oliver@cpan.org>
2014-05-17 19:06:22 +0100

    new DefaultSettings package, and mv queue to be key of workers

--- Netdisco/lib/App/Netdisco.pm
+++ Netdisco/lib/App/Netdisco.pm
@@ -7,58 +7,7 @@ use 5.010_000;
 our $VERSION = '2.027003';
 
 use App::Netdisco::Environment;
-use Dancer ':script';
-
-# set up database schema config from simple config vars
-if (ref {} eq ref setting('database')) {
-    my $name = (setting('database')->{name} || 'netdisco');
-    my $host = setting('database')->{host};
-    my $user = setting('database')->{user};
-    my $pass = setting('database')->{pass};
-
-    my $dsn = "dbi:Pg:dbname=${name}";
-    $dsn .= ";host=${host}" if $host;
-
-    # set up the netdisco schema now we have access to the config
-    # but only if it doesn't exist from an earlier config style
-    setting('plugins')->{DBIC}->{netdisco} ||= {
-        dsn  => $dsn,
-        user => $user,
-        password => $pass,
-        options => {
-            AutoCommit => 1,
-            RaiseError => 1,
-            auto_savepoint => 1,
-        },
-        schema_class => 'App::Netdisco::DB',
-    };
-
-}
-
-# static configuration for the in-memory local job queue
-setting('plugins')->{DBIC}->{daemon} = {
-    dsn => 'dbi:SQLite:dbname=:memory:',
-    options => {
-        AutoCommit => 1,
-        RaiseError => 1,
-        sqlite_use_immediate_transaction => 1,
-    },
-    schema_class => 'App::Netdisco::Daemon::DB',
-};
-
-# force skipped DNS resolution, if unset
-setting('dns')->{no} ||= ['fe80::/64','169.254.0.0/16'];
-setting('dns')->{hosts_file} ||= '/etc/hosts';
-
-# housekeeping expire used to be called expiry
-setting('housekeeping')->{expire} ||= setting('housekeeping')->{expiry}
-  if setting('housekeeping') and exists setting('housekeeping')->{expiry};
-
-# set max outstanding requests for AnyEvent::DNS
-$ENV{'PERL_ANYEVENT_MAX_OUTSTANDING_DNS'}
-  = setting('dns')->{max_outstanding} || 10;
-$ENV{'PERL_ANYEVENT_HOSTS'}
-  = setting('dns')->{hosts_file} || '/etc/hosts';
+use App::Netdisco::DefaultSettings;
 
 =head1 NAME
 
