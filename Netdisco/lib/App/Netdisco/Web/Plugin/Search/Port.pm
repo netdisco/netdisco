4f7a2097fc53f5bed56f73f2d5376fa761e001bb
Eric A. Miller <emiller@cpan.org>
2014-06-02 22:44:23 -0400

    DataTables for port search results tab Use device_port_vlan table to search for VLANs rather than the device_port table

--- Netdisco/lib/App/Netdisco/Web/Plugin/Search/Port.pm
+++ Netdisco/lib/App/Netdisco/Web/Plugin/Search/Port.pm
@@ -7,39 +7,57 @@ use Dancer::Plugin::Auth::Extensible;
 use App::Netdisco::Web::Plugin;
 use App::Netdisco::Util::Web 'sql_match';
 
-register_search_tab({ tag => 'port', label => 'Port', provides_csv => 1 });
+register_search_tab( { tag => 'port', label => 'Port', provides_csv => 1 } );
 
 # device ports with a description (er, name) matching
 get '/ajax/content/search/port' => require_login sub {
     my $q = param('q');
-    send_error('Missing query', 400) unless $q;
-    my $set;
+    send_error( 'Missing query', 400 ) unless $q;
+    my $rs;
 
-    if ($q =~ m/^\d+$/) {
-        $set = schema('netdisco')->resultset('DevicePort')
-          ->search({vlan => $q});
+    if ( $q =~ m/^\d+$/ ) {
+        $rs
+            = schema('netdisco')->resultset('DevicePort')
+            ->columns( [qw/ ip port name descr /] )->search(
+            { "port_vlans.vlan" => $q },
+            {   '+columns' => [qw/ device.dns device.name port_vlans.vlan /],
+                join       => [qw/ port_vlans device /]
+            }
+            );
     }
     else {
-        my ($likeval, $likeclause) = sql_match($q);
+        my ( $likeval, $likeclause ) = sql_match($q);
 
-        $set = schema('netdisco')->resultset('DevicePort')
-          ->search({-or => [
-                      {name => (param('partial') ? $likeclause : $q)},
-                      (length $q == 17 ? {mac => $q}
-                                       : \['mac::text ILIKE ?', $likeval]),
-                    ]});
+        $rs
+            = schema('netdisco')->resultset('DevicePort')
+            ->columns( [qw/ ip port name descr /] )->search(
+            {   -or => [
+                    { "me.name" => ( param('partial') ? $likeclause : $q ) },
+                    (   length $q == 17
+                        ? { "me.mac" => $q }
+                        : \[ 'me.mac::text ILIKE ?', $likeval ]
+                    ),
+                ]
+            },
+            {   '+columns' => [qw/ device.dns device.name port_vlans.vlan /],
+                join       => [qw/ port_vlans device /]
+            }
+            );
     }
-    return unless $set->count;
 
-    if (request->is_ajax) {
-        template 'ajax/search/port.tt', {results => $set},
-          { layout => undef };
+    my @results = $rs->hri->all;
+    return unless scalar @results;
+
+    if ( request->is_ajax ) {
+        my $json = to_json( \@results );
+        template 'ajax/search/port.tt', { results => $json },
+            { layout => undef };
     }
     else {
         header( 'Content-Type' => 'text/comma-separated-values' );
-        template 'ajax/search/port_csv.tt', {results => $set},
-          { layout => undef };
+        template 'ajax/search/port_csv.tt', { results => \@results },
+            { layout => undef };
     }
 };
 
-true;
+1;
