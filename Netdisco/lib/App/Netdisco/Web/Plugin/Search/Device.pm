a34094cc6c3c84b5d4f39b208d0217aaea76ab71
Eric A. Miller <emiller@cpan.org>
2014-06-02 22:41:47 -0400

    DataTables for device search results tab

--- Netdisco/lib/App/Netdisco/Web/Plugin/Search/Device.pm
+++ Netdisco/lib/App/Netdisco/Web/Plugin/Search/Device.pm
@@ -8,40 +8,46 @@ use List::MoreUtils ();
 
 use App::Netdisco::Web::Plugin;
 
-register_search_tab({ tag => 'device', label => 'Device', provides_csv => 1 });
-
-my $headers = ['Device','Contact','Location','System Name','Model','OS Version','Management IP','Serial'];
+register_search_tab(
+    { tag => 'device', label => 'Device', provides_csv => 1 } );
 
 # device with various properties or a default match-all
 get '/ajax/content/search/device' => require_login sub {
-    my $has_opt = List::MoreUtils::any {param($_)}
-      qw/name location dns ip description model os_ver vendor layers/;
-    my $set;
+    my $has_opt = List::MoreUtils::any { param($_) }
+    qw/name location dns ip description model os_ver vendor layers/;
+    my $rs;
 
     if ($has_opt) {
-        $set = schema('netdisco')->resultset('Device')
-          ->with_times->search_by_field(scalar params);
+        $rs = schema('netdisco')->resultset('Device')->columns(
+            [   "ip",       "dns",   "name",   "contact",
+                "location", "model", "os_ver", "serial"
+            ]
+        )->with_times->search_by_field( scalar params );
     }
     else {
         my $q = param('q');
-        send_error('Missing query', 400) unless $q;
+        send_error( 'Missing query', 400 ) unless $q;
 
-        $set = schema('netdisco')->resultset('Device')
-          ->with_times->search_fuzzy($q);
+        $rs = schema('netdisco')->resultset('Device')->columns(
+            [   "ip",       "dns",   "name",   "contact",
+                "location", "model", "os_ver", "serial"
+            ]
+        )->with_times->search_fuzzy($q);
     }
-    return unless $set->count;
 
-    if (request->is_ajax) {
-        template 'ajax/search/device.tt', {results => $set},
-          { layout => undef };
+    my @results = $rs->hri->all;
+    return unless scalar @results;
+
+    if ( request->is_ajax ) {
+        my $json = to_json( \@results );
+        template 'ajax/search/device.tt', { results => $json },
+            { layout => undef };
     }
     else {
         header( 'Content-Type' => 'text/comma-separated-values' );
-        template 'ajax/search/device_csv.tt', {
-          results => $set,
-          headers => $headers,
-        }, { layout => undef };
+        template 'ajax/search/device_csv.tt', { results => \@results, },
+            { layout => undef };
     }
 };
 
-true;
+1;
