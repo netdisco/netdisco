f13633283332d0a4244fcf7a10438efd3ff47e5d
Eric A. Miller <emiller@cpan.org>
2014-06-19 19:00:37 -0400

    DataTables for device PoE status report

--- Netdisco/lib/App/Netdisco/Web/Plugin/Report/DevicePoeStatus.pm
+++ Netdisco/lib/App/Netdisco/Web/Plugin/Report/DevicePoeStatus.pm
@@ -3,6 +3,7 @@ package App::Netdisco::Web::Plugin::Report::DevicePoeStatus;
 use Dancer ':syntax';
 use Dancer::Plugin::DBIC;
 use Dancer::Plugin::Auth::Extensible;
+use App::Netdisco::Util::ExpandParams 'expand_hash';
 
 use App::Netdisco::Web::Plugin;
 
@@ -14,20 +15,45 @@ register_report(
     }
 );
 
-get '/ajax/content/report/devicepoestatus' => require_login sub {
-    my $results = schema('netdisco')->resultset('Device')
-        ->with_poestats_as_hashref;
+get '/ajax/content/report/devicepoestatus/data' => require_role admin => sub {
+    send_error( 'Missing parameter', 400 )
+        unless ( param('draw') && param('draw') =~ /\d+/ );
 
-    return unless scalar $results;
+    my $rs = schema('netdisco')->resultset('Virtual::DevicePoeStatus');
+
+    my $exp_params = expand_hash( scalar params );
+
+    my $recordsTotal = $rs->count;
+
+    my @data = $rs->get_datatables_data($exp_params)->hri->all;
+
+    my $recordsFiltered = $rs->get_datatables_filtered_count($exp_params);
+
+    content_type 'application/json';
+    return to_json(
+        {   draw            => int( param('draw') ),
+            recordsTotal    => int($recordsTotal),
+            recordsFiltered => int($recordsFiltered),
+            data            => \@data,
+        }
+    );
+};
+
+get '/ajax/content/report/devicepoestatus' => require_login sub {
 
     if ( request->is_ajax ) {
-        template 'ajax/report/devicepoestatus.tt', { results => $results, },
-            { layout => undef };
+        template 'ajax/report/devicepoestatus.tt', {}, { layout => undef };
     }
     else {
+        my @results
+            = schema('netdisco')->resultset('Virtual::DevicePoeStatus')
+            ->hri->all;
+
+        return unless scalar @results;
+
         header( 'Content-Type' => 'text/comma-separated-values' );
         template 'ajax/report/devicepoestatus_csv.tt',
-            { results => $results, },
+            { results => \@results, },
             { layout  => undef };
     }
 };
