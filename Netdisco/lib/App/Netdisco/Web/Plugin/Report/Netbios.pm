89de64b20dca08e1194073a891e9d5050fd9447f
Eric A. Miller <emiller@cpan.org>
2014-06-19 19:02:28 -0400

    Change node netbios report by domain to use server-side processing

--- Netdisco/lib/App/Netdisco/Web/Plugin/Report/Netbios.pm
+++ Netdisco/lib/App/Netdisco/Web/Plugin/Report/Netbios.pm
@@ -3,6 +3,7 @@ package App::Netdisco::Web::Plugin::Report::Netbios;
 use Dancer ':syntax';
 use Dancer::Plugin::DBIC;
 use Dancer::Plugin::Auth::Extensible;
+use App::Netdisco::Util::ExpandParams 'expand_hash';
 
 use App::Netdisco::Web::Plugin;
 
@@ -34,32 +35,63 @@ hook 'before_template' => sub {
     }
 };
 
-get '/ajax/content/report/netbios' => require_login sub {
+get '/ajax/content/report/netbios/data' => require_login sub {
+    send_error( 'Missing parameter', 400 )
+        unless ( param('draw') && param('draw') =~ /\d+/ );
 
     my $domain = param('domain');
 
     my $rs = schema('netdisco')->resultset('NodeNbt');
 
-    if ( defined $domain ) {
+    my $search = $domain eq 'blank' ? '' : $domain;
+    $rs = $rs->search( { domain => $search } )
+        ->order_by( [ { -asc => 'domain' }, { -desc => 'time_last' } ] );
+
+    my $exp_params = expand_hash( scalar params );
+
+    my $recordsTotal = $rs->count;
+
+    my @data = $rs->get_datatables_data($exp_params)->hri->all;
+
+    my $recordsFiltered = $rs->get_datatables_filtered_count($exp_params);
+
+    content_type 'application/json';
+    return to_json(
+        {   draw            => int( param('draw') ),
+            recordsTotal    => int($recordsTotal),
+            recordsFiltered => int($recordsFiltered),
+            data            => \@data,
+        }
+    );
+};
+
+get '/ajax/content/report/netbios' => require_login sub {
+
+    my $domain = param('domain');
+
+    my $rs = schema('netdisco')->resultset('NodeNbt');
+    my @results;
+
+    if ( defined $domain && !request->is_ajax ) {
         my $search = $domain eq 'blank' ? '' : $domain;
-        $rs
+        @results
             = $rs->search( { domain => $search } )
             ->order_by( [ { -asc => 'domain' }, { -desc => 'time_last' } ] )
-            ->hri;
+            ->hri->all;
+
+        return unless scalar @results;
     }
-    else {
-        $rs = $rs->search(
+    elsif ( !defined $domain ) {
+        @results = $rs->search(
             {},
             {   select   => [ 'domain', { count => 'domain' } ],
                 as       => [qw/ domain count /],
                 group_by => [qw/ domain /]
             }
-        )->order_by( { -desc => 'count' } )->hri;
+        )->order_by( { -desc => 'count' } )->hri->all;
 
+        return unless scalar @results;
     }
-
-    my @results = $rs->all;
-    return unless scalar @results;
 
     if ( request->is_ajax ) {
         my $json = to_json( \@results );
