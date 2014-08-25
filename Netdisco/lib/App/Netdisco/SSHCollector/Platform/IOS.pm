b59e844d57b786b0b15082c6119a33a8731b7d34
Oliver Gorwits <oliver@cpan.org>
2014-08-25 10:13:18 +0100

    netdisco-sshcollector script to get ARP data on devices without SNMP

---
+++ Netdisco/lib/App/Netdisco/SSHCollector/Platform/IOS.pm
@@ -0,0 +1,55 @@
+package App::Netdisco::SSHCollector::Platform::IOS;
+
+# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4:
+
+=head1 NAME
+
+App::Netdisco::SSHCollector::Platform::IOS
+
+=head1 DESCRIPTION
+
+Collect ARP entries from Cisco IOS devices.
+
+=cut
+
+use strict;
+use warnings;
+
+use Dancer ':script';
+use Data::Printer;
+use Moo;
+
+=head1 PUBLIC METHODS
+
+=over 4
+
+=item B<arpnip($host, $ssh)>
+
+Retrieve ARP entries from device. C<$host> is the hostname or IP address
+of the device. C<$ssh> is a Net::OpenSSH connection to the device.
+
+Returns an array of hashrefs in the format { mac => MACADDR, ip => IPADDR }.
+
+=cut
+
+sub arpnip {
+    my ($self, $hostlabel, $ssh, @args) = @_;
+
+    debug "$hostlabel $$ arpnip()";
+    my @data = $ssh->capture("show ip arp");
+
+    chomp @data;
+    my @arpentries;
+
+    # Internet  172.16.20.15   13   0024.b269.867d  ARPA FastEthernet0/0.1
+    foreach my $line (@data) {
+        next unless $line =~ m/^Internet/;
+        my @fields = split m/\s+/, $line;
+
+        push @arpentries, { mac => $fields[3], ip => $fields[1] };
+    }
+
+    return @arpentries;
+}
+
+1;
