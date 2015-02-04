e99f5ee410b32b222fe74156a998fe0b94da5a3a
Oliver Gorwits <oliver@cpan.org>
2015-02-04 21:43:44 +0000

    [#199] Missing schema changes when user has no permissions on DB
    This fixes many glitches with the schema deployment:
    - silently ignoring real failure modes such as user not having
    correct permissions on the DB
    - a couple of broken upgrade steps
    - adds --reset so schema can be fully redeployed

--- Netdisco/lib/App/Netdisco/DB/schema_versions/App-Netdisco-DB-39-40-PostgreSQL.sql
+++ Netdisco/lib/App/Netdisco/DB/schema_versions/App-Netdisco-DB-39-40-PostgreSQL.sql
@@ -12,4 +12,6 @@
 
 BEGIN;
 
+DELETE FROM node AS n1 USING (SELECT mac, switch, port from node GROUP BY mac, switch, port HAVING count(*) > 1) n2 WHERE n1.mac = n2.mac AND n1.switch = n2.switch AND n1.port = n2.port AND n1.vlan = '0';
+
 COMMIT;
