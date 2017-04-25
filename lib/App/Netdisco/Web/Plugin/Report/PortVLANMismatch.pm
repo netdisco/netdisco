3ec84161ae851325252cf39a764f46b32cdc95c2
Oliver Gorwits <oliver@cpan.org>
2017-04-25 12:26:09 +0100

    New report for Port VLAN Mismatches (M. Bernstein)

---
+++ lib/App/Netdisco/Web/Plugin/Report/PortVLANMismatch.pm
@@ -0,0 +1,33 @@
+package App::Netdisco::Web::Plugin::Report::PortVLANMismatch;
+
+use Dancer ':syntax';
+use Dancer::Plugin::DBIC;
+use Dancer::Plugin::Auth::Extensible;
+
+use App::Netdisco::Web::Plugin;
+
+register_report(
+    {   category => 'Port',
+        tag      => 'portvlanmismatch',
+        label    => 'Port VLAN Mismatches',
+        provides_csv => 1,
+    }
+);
+
+get '/ajax/content/report/portvlanmismatch' => require_login sub {
+    return unless schema('netdisco')->resultset('Device')->count;
+    my @results = schema('netdisco')->resultset('Virtual::PortVLANMismatch')->hri->all;
+
+    if (request->is_ajax) {
+        my $json = to_json (\@results);
+        template 'ajax/report/portvlanmismatch.tt', { results => $json },
+            { layout => undef };
+    }
+    else {
+        header( 'Content-Type' => 'text/comma-separated-values' );
+        template 'ajax/report/portvlanmismatch.tt', { results => \@results, },
+            { layout => undef };
+    }
+};
+
+1;
