6244cece1e634990bbfb43ba6fedbc2e12e65e25
Oliver Gorwits <oliver@cpan.org>
2018-01-01 01:19:03 +0000

    update copyright date

--- lib/App/Netdisco.pm
+++ lib/App/Netdisco.pm
@@ -307,7 +307,7 @@ built upon.
 
 =head1 COPYRIGHT AND LICENSE
  
-This software is copyright (c) 2012-2015 by The Netdisco Developer Team.
+This software is copyright (c) 2011-2018 by The Netdisco Developer Team.
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
