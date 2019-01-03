80c224afed55856bd3f5cba50afd2483c5a65337
inphobia <nick.nauwelaerts@aquafin.be>
2019-01-03 19:28:25 +0100

    update manifest, get worker closer to functioning

--- lib/App/Netdisco/Web/Plugin/Device/Vlans.pm
+++ lib/App/Netdisco/Web/Plugin/Device/Vlans.pm
@@ -8,7 +8,7 @@ use Dancer::Plugin::Ajax;
 use Dancer::Plugin::DBIC;
 use Dancer::Plugin::Auth::Extensible;
 
-use App::Netdisco::Util::Web (); # for sort_module
+#use App::Netdisco::Util::Web (); # for sort_module
 use App::Netdisco::Web::Plugin;
 
 register_device_tab({ tag => 'vlans', label => 'Vlans' });
@@ -18,16 +18,25 @@ ajax '/ajax/content/device/vlans' => require_login sub {
 
     my $device = schema('netdisco')->resultset('Device')
       ->search_for_device($q) or send_error('Bad device', 400);
-    my @set = $device->vlans->search({}, {order_by => { -asc => [qw/vlan/] }});
+    my @results = $device->vlans->search( {}, { order_by => 'vlan' } )->hri->all;
+
+    return unless scalar @results;
 
     # sort modules (empty set would be a 'no records' msg)
-    my $results = &App::Netdisco::Util::Web::sort_vlans( \@set );
-    return unless scalar %$results;
+    #my $results = &App::Netdisco::Util::Web::sort_vlans( \@set );
+    #return unless scalar %$results;
 
-    content_type('text/html');
-    template 'ajax/device/vlans.tt', {
-      nodes => $results,
-    }, { layout => undef };
+
+    if (request->is_ajax) {
+      my $json = to_json( \@results );
+      template 'ajax/device/vlans.tt', { results => $json },
+        { layout => undef };
+    }
+
+#    content_type('text/html');
+#    template 'ajax/device/vlans.tt', {
+#      nodes => $results,
+#    }, { layout => undef };
 };
 
 true;
