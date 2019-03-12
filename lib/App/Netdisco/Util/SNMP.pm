b9218d91c8f3cb17ce1a106386ec7f71dcdc3744
Oliver Gorwits <ollyg@users.noreply.github.com>
2019-03-12 13:13:06 +0000

    Support for external command to provide device_auth stanza (#532)
    * separate out generic device auth to DeviceAuth module
    
    * change config name to get_credentials
    
    * add debug lines and tested

--- lib/App/Netdisco/Util/SNMP.pm
+++ lib/App/Netdisco/Util/SNMP.pm
@@ -1,14 +1,11 @@
 package App::Netdisco::Util::SNMP;
 
 use Dancer qw/:syntax :script/;
-use App::Netdisco::Util::DNS 'hostname_from_ip';
-use App::Netdisco::Util::Permission ':all';
+use App::Netdisco::Util::DeviceAuth 'get_external_credentials';
 
 use base 'Exporter';
 our @EXPORT = ();
-our @EXPORT_OK = qw/
-  fixup_device_auth get_communities snmp_comm_reindex
-/;
+our @EXPORT_OK = qw/ get_communities snmp_comm_reindex /;
 our %EXPORT_TAGS = (all => \@EXPORT_OK);
 
 =head1 NAME
@@ -23,72 +20,6 @@ There are no default exports, however the C<:all> tag will export all
 subroutines.
 
 =head1 EXPORT_OK
-
-=head2 fixup_device_auth
-
-Rebuilds the C<device_auth> config with missing defaults and other fixups for
-config changes over time. Returns a list which can replace C<device_auth>.
-
-=cut
-
-sub fixup_device_auth {
-  my $config = (setting('snmp_auth') || setting('device_auth'));
-  my @new_stanzas = ();
-
-  # new style snmp config
-  foreach my $stanza (@$config) {
-    # user tagged
-    my $tag = '';
-    if (1 == scalar keys %$stanza) {
-      $tag = (keys %$stanza)[0];
-      $stanza = $stanza->{$tag};
-
-      # corner case: untagged lone community
-      if ($tag eq 'community') {
-          $tag = $stanza;
-          $stanza = {community => $tag};
-      }
-    }
-
-    # defaults
-    $stanza->{tag} ||= $tag;
-    $stanza->{read} = 1 if !exists $stanza->{read};
-    $stanza->{no}   ||= [];
-    $stanza->{only} ||= ['any'];
-
-    die "error: config: snmpv2 community in device_auth must be single item, not list\n"
-      if ref $stanza->{community};
-
-    die "error: config: stanza in device_auth must have a tag\n"
-      if not $stanza->{tag} and exists $stanza->{user};
-
-    push @new_stanzas, $stanza
-  }
-
-  # legacy config 
-  # note: read strings tried before write
-  # note: read-write is no longer used for read operations
-
-  push @new_stanzas, map {{
-    read => 1, write => 0,
-    no => [], only => ['any'],
-    community => $_,
-  }} @{setting('community') || []};
-
-  push @new_stanzas, map {{
-    write => 1, read => 0,
-    no => [], only => ['any'],
-    community => $_,
-  }} @{setting('community_rw') || []};
-
-  foreach my $stanza (@new_stanzas) {
-    $stanza->{driver} ||= 'snmp'
-      if exists $stanza->{community}
-         or exists $stanza->{user};
-  }
-
-  return @new_stanzas;
-}
 
 =head2 get_communities( $device, $mode )
 
@@ -106,8 +37,7 @@ sub get_communities {
   my @communities = ();
 
   # first of all, use external command if configured
-  push @communities, _get_external_community($device, $mode)
-    if setting('get_community') and length setting('get_community');
+  push @communities, get_external_credentials($device, $mode);
 
   # last known-good by tag
   my $tag_name = 'snmp_auth_tag_'. $mode;
@@ -143,46 +73,6 @@ sub get_communities {
        and (not $stored_tag or !exists $seen_tags->{ $stored_tag });
 
   return ( @communities, @$config );
-}
-
-sub _get_external_community {
-  my ($device, $mode) = @_;
-  my $cmd = setting('get_community');
-  my $ip = $device->ip;
-  my $host = ($device->dns || hostname_from_ip($ip) || $ip);
-
-  if (defined $cmd and length $cmd) {
-      # replace variables
-      $cmd =~ s/\%HOST\%/$host/egi;
-      $cmd =~ s/\%IP\%/$ip/egi;
-
-      my $result = `$cmd`; # BACKTICKS
-      return () unless defined $result and length $result;
-
-      my @lines = split (m/\n/, $result);
-      foreach my $line (@lines) {
-          if ($line =~ m/^community\s*=\s*(.*)\s*$/i) {
-              if (length $1 and $mode eq 'read') {
-                  return map {{
-                    read => 1,
-                    only => [$device->ip],
-                    community => $_,
-                  }} split(m/\s*,\s*/,$1);
-              }
-          }
-          elsif ($line =~ m/^setCommunity\s*=\s*(.*)\s*$/i) {
-              if (length $1 and $mode eq 'write') {
-                  return map {{
-                    write => 1,
-                    only => [$device->ip],
-                    community => $_,
-                  }} split(m/\s*,\s*/,$1);
-              }
-          }
-      }
-  }
-
-  return ();
 }
 
 =head2 snmp_comm_reindex( $snmp, $device, $vlan )
