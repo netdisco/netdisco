5045ceebff98fb515c921f0212977d4f565adfb7
Ryan Gasik <pyro3d@gmail.com>
2019-03-19 22:50:07 -0700

    Add IS-IS Support

--- lib/App/Netdisco/Worker/Plugin/Discover/Neighbors/Routed.pm
+++ lib/App/Netdisco/Worker/Plugin/Discover/Neighbors/Routed.pm
@@ -18,16 +18,20 @@ register_worker({ phase => 'main', driver => 'snmp' }, sub {
 
   my $ospf_peers   = $snmp->ospf_peers || {};
   my $ospf_routers = $snmp->ospf_peer_id || {};
+  my $isis_peers   = $snmp->isis_peers || {};
+  my $isis_routers = $snmp->isis_adj_id || {};
   my $bgp_peers    = $snmp->bgp_peer_addr || {};
   my $eigrp_peers  = $snmp->eigrp_peers || {};
 
-  return Status->info(" [$device] neigh - no BGP, OSPF, or EIGRP peers")
+  return Status->info(" [$device] neigh - no BGP, OSPF, IS-IS, or EIGRP peers")
     unless ((scalar values %$ospf_peers) or (scalar values %$ospf_routers)
-            or (scalar values %$bgp_peers) or (scalar values %$eigrp_peers));
+            or (scalar values %$bgp_peers) or (scalar values %$eigrp_peers))
+            or (scalar values %$isis_peers);
 
   my $count = 0;
   foreach my $ip ((values %$ospf_peers), (values %$ospf_routers),
-                  (values %$bgp_peers), (values %$eigrp_peers)) {
+                  (values %$bgp_peers), (values %$eigrp_peers),
+                  (values %$isis_peers)) {
     my $peer = get_device($ip);
     next if $peer->in_storage or not is_discoverable($peer);
     next if vars->{'queued'}->{$ip};
