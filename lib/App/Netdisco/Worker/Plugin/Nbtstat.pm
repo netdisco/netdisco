635a07b19b0cd9a51e056ae83dd5c5fbab20c334
Christian Ramseyer <ramseyer@netnea.com>
2019-08-20 13:50:39 +0200

    Make Worker/Plugin/Nbtstat us is_nbtstatable

--- lib/App/Netdisco/Worker/Plugin/Nbtstat.pm
+++ lib/App/Netdisco/Worker/Plugin/Nbtstat.pm
@@ -4,7 +4,7 @@ use Dancer ':syntax';
 use App::Netdisco::Worker::Plugin;
 use aliased 'App::Netdisco::Worker::Status';
 
-use App::Netdisco::Util::Device 'is_macsuckable';
+use App::Netdisco::Util::Node 'is_nbtstatable';
 
 register_worker({ phase => 'check' }, sub {
   my ($job, $workerconf) = @_;
@@ -12,10 +12,11 @@ register_worker({ phase => 'check' }, sub {
   return Status->error('nbtstat failed: unable to interpret device param')
     unless defined $job->device;
 
-  return Status->info(sprintf 'nbtstat skipped: %s is not macsuckable', $job->device->ip)
-    unless is_macsuckable($job->device);
+  return Status->info(sprintf 'nbtstat skipped: %s is not nbtstable', $job->device->ip)
+    unless is_nbtstatable($job->device->ip);
 
   return Status->done('Nbtstat is able to run.');
 });
 
 true;
+
