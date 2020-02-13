912a971a75fcf288cb433e478868862bf9a36a12
Oliver Gorwits <oliver@cpan.org>
2020-02-13 10:23:07 +0000

    tidy the Clavister module

--- lib/App/Netdisco/SSHCollector/Platform/Clavister.pm
+++ lib/App/Netdisco/SSHCollector/Platform/Clavister.pm
@@ -1,38 +1,37 @@
 package App::Netdisco::SSHCollector::Platform::Clavister;
 
-# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4:
-
 =head1 NAME
+
 App::Netdisco::SSHCollector::Platform::Clavister
+
 =head1 DESCRIPTION
+
 Collect ARP entries from Clavister firewalls.
 These devices does not expose mac table through snmp.
+
 =cut
 
 use strict;
 use warnings;
-use Data::Dumper;
 
-sub new {
-    my $proto = shift;
-    my $class = ref($proto) || $proto;
-    my $self  = {};
-    bless ($self, $class);
-    return $self;
-}
+use Dancer ':script';
+use Moo;
 
 =head1 PUBLIC METHODS
+
 =over 4
+
 =item B<arpnip($host, $ssh)>
+
 Retrieve ARP entries from device. C<$host> is the hostname or IP address
 of the device. C<$ssh> is a Net::OpenSSH connection to the device.
 Returns an array of hashrefs in the format { mac => MACADDR, ip => IPADDR }.
+
 =cut
+
 sub arpnip {
-
     my ($self, $hostlabel, $ssh, @args) = @_;
-
-    print "$hostlabel $$ arpnip()\n";
+    debug "$hostlabel $$ arpnip()";
 
     my @data = $ssh->capture("neighborcache");
     chomp @data;
@@ -49,4 +48,5 @@ sub arpnip {
     }
     return @arpentries;
 }
+
 1;
