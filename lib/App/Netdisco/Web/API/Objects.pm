19fde39191fac8a6fbcc8eecf009655901b8b21a
Oliver Gorwits <oliver@cpan.org>
2020-04-12 18:13:14 +0100

    add device_port api endpoint

--- lib/App/Netdisco/Web/API/Objects.pm
+++ lib/App/Netdisco/Web/API/Objects.pm
@@ -1,29 +1,51 @@
 package App::Netdisco::Web::API::Objects;
 
 use Dancer ':syntax';
-use Dancer::Plugin::Res;
 use Dancer::Plugin::DBIC;
 use Dancer::Plugin::Swagger;
 use Dancer::Plugin::Auth::Extensible;
 
-use App::Netdisco::Web::Plugin;
+use Try::Tiny;
 
 swagger_path {
   tags => ['Objects'],
   description => 'Returns a row from the device table',
   parameters  => [
-    device => {
+    ip => {
       description => 'Canonical IP of the Device. Use Search methods to find this.',
       required => 1,
       in => 'path',
     },
   ],
   responses => { default => {} },
-}, get '/api/v1/device/:device' => require_role api => sub {
-  my $dev = params->{device};
-  my $device = schema('netdisco')->resultset('Device')
-    ->find($dev) or send_error('Bad Device', 404);
+}, get '/api/v1/object/device/:ip' => require_role api => sub {
+  my $device = try { schema('netdisco')->resultset('Device')
+    ->find( params->{ip} ) } or send_error('Bad Device', 404);
   return to_json $device->TO_JSON;
+};
+
+swagger_path {
+  tags => ['Objects'],
+  description => 'Returns a row from the device_port table',
+  path => '/api/v1/object/device/{ip}/port/{port}',
+  parameters  => [
+    ip => {
+      description => 'Canonical IP of the Device. Use Search methods to find this.',
+      required => 1,
+      in => 'path',
+    },
+    port => {
+      description => 'Name of the port. Use the ".../device/:ip/ports" method to find these.',
+      required => 1,
+      in => 'path',
+    },
+  ],
+  responses => { default => {} },
+}, get qr{/api/v1/object/device/(?<ip>.*)/port/(?<port>.*)} => require_role api => sub {
+  my $params = captures;
+  my $port = try { schema('netdisco')->resultset('DevicePort')
+    ->find( $$params{port}, $$params{ip} ) } or send_error('Bad Device or Port', 404);
+  return to_json $port->TO_JSON;
 };
 
 true;
