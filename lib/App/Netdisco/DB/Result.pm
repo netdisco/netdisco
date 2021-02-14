2e6bfa42b45e752bacbf13824feecab3889184c3
Oliver Gorwits <oliver@cpan.org>
2021-02-14 14:04:08 +0000

    fix error when using API /api/v1/object/device/ip/port_vlans

--- lib/App/Netdisco/DB/Result.pm
+++ lib/App/Netdisco/DB/Result.pm
@@ -5,7 +5,21 @@ use warnings;
 
 use base 'DBIx::Class::Core';
 
-__PACKAGE__->load_components(qw{Helper::Row::ToJSON});
+BEGIN {
+  no warnings 'redefine';
+  __PACKAGE__->load_components(qw{Helper::Row::ToJSON});
+
+  # this replacement will avoid the issue of relation names which override
+  # field names, causing TO_JSON to return object instances, breaking to_json
+  *DBIx::Class::Helper::Row::ToJSON::TO_JSON = sub {
+      my $self = shift;
+      my $columns_info = $self->columns_info($self->serializable_columns);
+      my $columns_data = { $self->get_columns };
+      return {
+         map +($_ => $columns_data->{$_}), keys %$columns_info
+      };
+  };
+}
 
 # for DBIx::Class::Helper::Row::ToJSON
 # to allow text columns to be included in results
