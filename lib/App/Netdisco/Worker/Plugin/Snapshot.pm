643b9aff0a8490010ba2911f84775b9b4f1b4b3d
Oliver Gorwits <oliver@cpan.org>
2021-10-22 19:24:52 +0100

    can now bulkwalk and parse mibs report and store resolved walk in cache

--- lib/App/Netdisco/Worker/Plugin/Snapshot.pm
+++ lib/App/Netdisco/Worker/Plugin/Snapshot.pm
@@ -28,185 +28,61 @@ register_worker({ phase => 'main', driver => 'snmp' }, sub {
   my $snmp = App::Netdisco::Transport::SNMP->reader_for($device)
     or return Status->defer("snapshot failed: could not SNMP connect to $device");
 
-#  my $home = (setting('mibhome') || catdir(($ENV{NETDISCO_HOME} || $ENV{HOME}), 'netdisco-mibs'));
-#  my @report = read_lines(catfile($home, qw(EXTRAS reports all)), 'latin-1')
-#    or return Status->error("snapshot failed to read netdisco-mibs report file");
+  my %oidmap = getoidmap($device, $snmp);
+  # my %localstore = walker($device, $snmp);
+  # my %localstore = walker($device, $snmp, '.1.3.6.1.2.1.5.30.1.4'); # 16 rows
+  my %localstore = walker($device, $snmp, '.1.3.6.1.2.1.2.2.1.6');        # 22 rows, i_mac/ifPhysAddress
+  # my %localstore = walker($device, $snmp, '.1.3.6.1.2.1.1');        # 29 rows
+  # my %localstore = walker($device, $snmp, '.1.3.6.1.2.1.2.2.1');    # 484 rows
+  # my %localstore = walker($device, $snmp, '.1.3.6.1');              # 10205 rows
+
+  my (%tables, %leaves) = ((), ());
+  OID: foreach my $origoid (keys %localstore) {
+    my $oid = $origoid;
+    my $idx = '';
+
+    while (length($oid) and !exists $oidmap{$oid}) {
+      $oid =~ s/\.(\d+)$//;
+      $idx = ($idx ? "${1}.${idx}" : $1);
+    }
+
+    if (exists $oidmap{$oid}) {
+      $idx =~ s/^\.//;
+
+      if ($idx eq 0) {
+        $leaves{ $oidmap{$oid} } = $localstore{$origoid};
+      }
+      else {
+        $tables{ $oidmap{$oid} }->{$idx} = $localstore{$origoid};
+      }
+
+      # debug "snapshot $device - cached $oidmap{$oid}($idx)";
+      next OID;
+    }
+
+    debug "snapshot $device - missing OID $origoid in netdisco-mibs";
+  }
+
+  $snmp->_cache($_, $tables{$_}) for keys %tables;
+  $snmp->_cache($_, $leaves{$_}) for keys %leaves;
+
+#  # specific to this device's class
+#  my %aliases = (%{ $snmp->funcs() }, %{ $snmp->globals() });
+#  my %cache = %{ $snmp->cache() };
+#  p %aliases;
+#  p %cache;
 #
-#  my $oid = '.1';
-#  my $last_indent = 0;
-#  my %oidmap = ();
+#  # !!! poking SNMP::Info internals now
+#  foreach my $alias (keys %aliases) {
+#    my $target = $aliases{ $alias };
 #
-#  foreach my $line (@report) {
-#    my ($spaces, $leaf, $idx) = ($line =~ m/^(\s*)([-\w]+)\((\d+)\)/);
-#    next unless defined $spaces and defined $leaf and defined $idx;
-#    my $this_indent = length($spaces);
-#
-#    # update current OID
-#    if ($this_indent <= $last_indent) {
-#      my $step_back = (($last_indent - $this_indent) / 2);
-#      foreach (0 .. $step_back) { $oid =~ s/\.\d+$// }
+#    if (exists $cache{"_${target}"}) {
+#        $snmp->{"_${alias}"} = \$snmp->{"_${target}"};
 #    }
-#    $oid .= ".$idx";
-#
-#    # store what we have just seen
-#    $oidmap{$oid} = $leaf;
-#
-#    # and remember the indent
-#    $last_indent = $this_indent;
+#    elsif (exists $cache{store}->{$target}) {
+#        $snmp->{store}->{$alias} = \$snmp->{store}->{$target};
+#    }
 #  }
-#
-#  #p %oidmap;
-#  # FIXME
-#  return Status->done(
-#    sprintf "Snapshot data from %s", $device->ip);
-
-    my $REPEATERS = 20;
-    my $method = 'snmpwalk';
-    my $oid = '.1.3.6.1.2.1.2.2';
-
-    my $walker = sub {
-        my $self = $snmp;
-        my $sess = $self->session();
-        return unless defined $sess;
-
-        my $ver  = $self->snmp_ver();
-        my $attr = $method;
-
-        # We want the qualified leaf name so that we can
-        # specify the Module (MIB) in the case of private leaf naming
-        # conflicts.  Example: ALTEON-TIGON-SWITCH-MIB::agSoftwareVersion
-        # and ALTEON-CHEETAH-SWITCH-MIB::agSoftwareVersion
-        # Third argument to translateObj specifies the Module prefix
-
-        my $qual_leaf = SNMP::translateObj($oid,0,1) || '';
-
-        # We still want just the leaf since a SNMP get in the case of a
-        # partial fetch may strip the Module portion upon return.  We need
-        # the match to make sure we didn't leave the table during getnext
-        # requests
-
-        my ($leaf) = $qual_leaf =~ /::(.+)$/;
-
-        # If we weren't able to translate, we'll only have an OID
-        $leaf = $oid unless defined $leaf;
-
-        debug "SNMP::Info::_load_attr $method : $qual_leaf : $oid";
-        my $var = SNMP::Varbind->new( [$qual_leaf] );
-
-        # So devices speaking SNMP v.1 are not supposed to give out
-        # data from SNMP2, but most do.  Net-SNMP, being very precise
-        # will tell you that the SNMP OID doesn't exist for the device.
-        # They have a flag RetryNoSuch that is used for get() operations,
-        # but not for getnext().  We set this flag normally, and if we're
-        # using V1, let's try and fetch the data even if we get one of those.
-
-        my $localstore = undef;
-        my $errornum   = 0;
-        my %seen       = ();
-
-        my $vars = [];
-        my $bulkwalk_no
-            = $self->can('bulkwalk_no') ? $self->bulkwalk_no() : 0;
-        my $bulkwalk_on = defined $self->{BulkWalk} ? $self->{BulkWalk} : 1;
-        my $can_bulkwalk = $bulkwalk_on && !$bulkwalk_no;
-        my $repeaters = $self->{BulkRepeaters} || $REPEATERS;
-        my $bulkwalk = $can_bulkwalk && $ver != 1;
-        my $loopdetect
-            = defined $self->{LoopDetect} ? $self->{LoopDetect} : 1;
-
-        # Use BULKWALK if we can because its faster
-        if ( $bulkwalk && @$vars == 0 ) {
-            debug "SNMP::Info::_load_attr BULKWALK $qual_leaf";
-            ($vars) = $sess->bulkwalk( 0, $repeaters, $var );
-            if ( $sess->{ErrorNum} ) {
-                error "SNMP::Info::_load_attr: BULKWALK " . $sess->{ErrorStr};
-                return;
-            }
-            # p $vars->[10];
-        }
-
-        while ( !$errornum ) {
-            if ($bulkwalk) {
-                $var = shift @$vars or last;
-            }
-            else {
-
-                # GETNEXT instead of BULKWALK
-                debug "SNMP::Info::_load_attr GETNEXT $var";
-                $sess->getnext($var);
-                $errornum = $sess->{ErrorNum};
-            }
-
-            #use Data::Dumper;
-            #debug "SNMP::Info::_load_attr $method : leaf = $oid , var = ",
-            #        Dumper($var);
-            p $var;
-
-            # Check if we've left the requested subtree
-            #if ($var->[0] !~ /$leaf$/) {
-            #  debug "SNMP::Info::_load_attr $method : left the requested subtree";
-            #  last;
-            #}
-
-            my $iid = $var->[1];
-            my $val = $var->[2];
-
-            unless ( defined $iid ) {
-                error "SNMP::Info::_load_attr: $method not here";
-                next;
-            }
-
-           # Check if last element, V2 devices may report ENDOFMIBVIEW even if
-           # instance or object doesn't exist.
-            if ( $val eq 'ENDOFMIBVIEW' ) {
-                debug "SNMP::Info::_load_attr $method : ENDOFMIBVIEW";
-                last;
-            }
-
-            # Similarly for SNMPv1 - noSuchName return results in both $iid
-            # and $val being empty strings.
-            if ( $val eq '' and $iid eq '' ) {
-                debug "SNMP::Info::_load_attr $method : v1 noSuchName (1)";
-                last;
-            }
-
-            # Another check for SNMPv1 - noSuchName return may results in an $iid
-            # we've already seen and $val an empty string.  If we don't catch
-            # this here we erroneously report a loop below.
-            if ( defined $seen{$iid} and $seen{$iid} and $val eq '' ) {
-                debug "SNMP::Info::_load_attr $method : v1 noSuchName (2)";
-                last;
-            }
-
-            if ($loopdetect) {
-                # Check to see if we've already seen this IID (looping)
-                if ( defined $seen{$iid} and $seen{$iid} ) {
-                    error "Looping on: $method iid:$iid. ";
-                    last;
-                }
-                else {
-                    $seen{$iid}++;
-                }
-            }
-
-            if ( $val eq 'NOSUCHOBJECT' ) {
-                error "SNMP::Info::_load_attr: $method :  NOSUCHOBJECT";
-                next;
-            }
-            if ( $val eq 'NOSUCHINSTANCE' ) {
-                error "SNMP::Info::_load_attr: $method :  NOSUCHINSTANCE";
-                next;
-            }
-
-            $localstore->{$iid} = $val;
-        }
-
-        # Cache data
-        $self->_cache($attr, $localstore);
-        return $localstore;
-    };
-
-    $walker->();
 
 #  foreach my $method (qw/
 #    i_mac
@@ -224,12 +100,189 @@ register_worker({ phase => 'main', driver => 'snmp' }, sub {
       my ($key, $valueref) = @_;
       ($$valueref = encode_base64( $$valueref )) =~ s/\n$// if defined $_ and ref $_ eq q{};
   });
-  p $cache;
+  # p $cache;
 
   $job->subaction( encode_base64( nfreeze( $cache ) ) );
   return Status->done(
     sprintf "Snapshot data captured from %s", $device->ip);
 });
+
+
+# read in netdisco-mibs translation report and make an OID -> leafname map
+# takes about a second to run on my laptop
+sub getoidmap {
+  my ($device, $snmp, $oid) = @_;
+  $oid ||= '.1';
+
+  debug "snapshot $device - starting netdisco-mibs report parse";
+
+  my $home = (setting('mibhome') || catdir(($ENV{NETDISCO_HOME} || $ENV{HOME}), 'netdisco-mibs'));
+  my @report = read_lines(catfile($home, qw(EXTRAS reports all)), 'latin-1');
+
+  my %oidmap = ();
+  my $last_indent = 0;
+
+  foreach my $line (@report) {
+    my ($spaces, $leaf, $idx) = ($line =~ m/^(\s*)([-\w]+)\((\d+)\)/);
+    next unless defined $spaces and defined $leaf and defined $idx;
+    my $this_indent = length($spaces);
+
+    # update current OID
+    if ($this_indent <= $last_indent) {
+      my $step_back = (($last_indent - $this_indent) / 2);
+      foreach (0 .. $step_back) { $oid =~ s/\.\d+$// }
+    }
+    $oid .= ".$idx";
+
+    # store what we have just seen
+    $oidmap{$oid} = $leaf;
+
+    # and remember the indent
+    $last_indent = $this_indent;
+  }
+
+  debug sprintf "snapshot $device - parsed %d items from netdisco-mibs report",
+    scalar keys %oidmap;
+  return %oidmap;
+}
+
+# taken from SNMP::Info and adjusted to work on walks outside a single table
+sub walker {
+    my ($device, $snmp, $base) = @_;
+    $base ||= '.1';
+
+    my $sess = $snmp->session();
+    return unless defined $sess;
+
+    my $REPEATERS = 20;
+    my $ver  = $snmp->snmp_ver();
+
+    # We want the qualified leaf name so that we can
+    # specify the Module (MIB) in the case of private leaf naming
+    # conflicts.  Example: ALTEON-TIGON-SWITCH-MIB::agSoftwareVersion
+    # and ALTEON-CHEETAH-SWITCH-MIB::agSoftwareVersion
+    # Third argument to translateObj specifies the Module prefix
+
+    my $qual_leaf = SNMP::translateObj($base,0,1) || '';
+
+    # We still want just the leaf since a SNMP get in the case of a
+    # partial fetch may strip the Module portion upon return.  We need
+    # the match to make sure we didn't leave the table during getnext
+    # requests
+
+    my ($leaf) = $qual_leaf =~ /::(.+)$/;
+
+    # If we weren't able to translate, we'll only have an OID
+    $leaf = $base unless defined $leaf;
+
+    # debug "snapshot $device - $base translated as $qual_leaf";
+    my $var = SNMP::Varbind->new( [$qual_leaf] );
+
+    # So devices speaking SNMP v.1 are not supposed to give out
+    # data from SNMP2, but most do.  Net-SNMP, being very precise
+    # will tell you that the SNMP OID doesn't exist for the device.
+    # They have a flag RetryNoSuch that is used for get() operations,
+    # but not for getnext().  We set this flag normally, and if we're
+    # using V1, let's try and fetch the data even if we get one of those.
+
+    my %localstore = ();
+    my $errornum   = 0;
+    my %seen       = ();
+
+    my $vars = [];
+    my $bulkwalk_no
+        = $snmp->can('bulkwalk_no') ? $snmp->bulkwalk_no() : 0;
+    my $bulkwalk_on = defined $snmp->{BulkWalk} ? $snmp->{BulkWalk} : 1;
+    my $can_bulkwalk = $bulkwalk_on && !$bulkwalk_no;
+    my $repeaters = $snmp->{BulkRepeaters} || $REPEATERS;
+    my $bulkwalk = $can_bulkwalk && $ver != 1;
+    my $loopdetect
+        = defined $snmp->{LoopDetect} ? $snmp->{LoopDetect} : 1;
+
+    debug "snapshot $device - starting walk from $base";
+
+    # Use BULKWALK if we can because its faster
+    if ( $bulkwalk && @$vars == 0 ) {
+        ($vars) = $sess->bulkwalk( 0, $repeaters, $var );
+        if ( $sess->{ErrorNum} ) {
+            error "snapshot $device BULKWALK " . $sess->{ErrorStr};
+            return;
+        }
+    }
+
+    while ( !$errornum ) {
+        if ($bulkwalk) {
+            $var = shift @$vars or last;
+        }
+        else {
+            # GETNEXT instead of BULKWALK
+            # debug "snapshot $device GETNEXT $var";
+            $sess->getnext($var);
+            $errornum = $sess->{ErrorNum};
+        }
+
+        my $iid = $var->[1];
+        my $val = $var->[2];
+        my $oid = $var->[0] . (defined $iid ? ".${iid}" : '');
+
+        # debug "snapshot $device reading $oid";
+        # p $var;
+
+        unless ( defined $iid ) {
+            error "snapshot $device not here";
+            next;
+        }
+
+       # Check if last element, V2 devices may report ENDOFMIBVIEW even if
+       # instance or object doesn't exist.
+        if ( $val eq 'ENDOFMIBVIEW' ) {
+            debug "snapshot $device : ENDOFMIBVIEW";
+            last;
+        }
+
+        # Similarly for SNMPv1 - noSuchName return results in both $iid
+        # and $val being empty strings.
+        if ( $val eq '' and $iid eq '' ) {
+            debug "snapshot $device : v1 noSuchName (1)";
+            last;
+        }
+
+        # Another check for SNMPv1 - noSuchName return may results in an $oid
+        # we've already seen and $val an empty string.  If we don't catch
+        # this here we erroneously report a loop below.
+        if ( defined $seen{$oid} and $seen{$oid} and $val eq '' ) {
+            debug "snapshot $device : v1 noSuchName (2)";
+            last;
+        }
+
+        if ($loopdetect) {
+            # Check to see if we've already seen this IID (looping)
+            if ( defined $seen{$oid} and $seen{$oid} ) {
+                error "Looping on: oid:$oid. ";
+                last;
+            }
+            else {
+                $seen{$oid}++;
+            }
+        }
+
+        if ( $val eq 'NOSUCHOBJECT' ) {
+            error "snapshot $device :  NOSUCHOBJECT";
+            next;
+        }
+        if ( $val eq 'NOSUCHINSTANCE' ) {
+            error "snapshot $device :  NOSUCHINSTANCE";
+            next;
+        }
+
+        # debug "snapshot $device - retreived $oid : $val";
+        $localstore{$oid} = $val;
+    }
+
+    debug sprintf "snapshot $device - walked %d rows from $base",
+      scalar keys %localstore;
+    return %localstore;
+}
 
 true;
 __END__
