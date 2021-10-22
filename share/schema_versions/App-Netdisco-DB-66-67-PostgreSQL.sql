676abb0064d58de703f2914ec4a9d717ee413666
inphobia <nick.nauwelaerts@aquafin.be>
2021-10-22 05:57:30 +0200

    new version

--- share/schema_versions/App-Netdisco-DB-66-67-PostgreSQL.sql
+++ share/schema_versions/App-Netdisco-DB-66-67-PostgreSQL.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 UPDATE device SET model = 'pseudodevice' WHERE vendor = 'netdisco';
 
-UPDATE device SET os_ver = '2.49.5' WHERE vendor = 'netdisco' AND os_ver IS NULL;
+UPDATE device SET os_ver = '2.50.3' WHERE vendor = 'netdisco' AND os_ver IS NULL;
 
 UPDATE device SET os = 'netdisco' WHERE vendor = 'netdisco' AND os IS NULL;
 
