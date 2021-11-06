636aa07f2da7386a71c823fa0a41f4b7c75c682f
nick n <39005454+inphobia@users.noreply.github.com>
2021-11-06 22:52:59 +0100

    Give Pseudo Devices better OS, OS ver, Port Descr, Discovered, and DNS Name
    * started pseudodevice rework
    * give them a type, os, os_ver and num_ports
      * os_ver will be the netdisco version in which they were created
    * give their ports the same value for device_port.descr as device_port.port
    * add db update to fill out those fields if they are null, which is most likely
    * update last_discover if you make changes to the device
    * num_ports will not be updates by the db script, neither will device_port.descr
    
    * use device.name, not device.dns for pseudo
    
    * at least try and get reverse dns for pseudo, works if there is one
    
    * add db revision to manifest
    
    * take dns no config into account
    
    * new meta
    
    * new version
    
    Co-authored-by: Oliver Gorwits <ollyg@users.noreply.github.com>

--- share/schema_versions/App-Netdisco-DB-66-67-PostgreSQL.sql
+++ share/schema_versions/App-Netdisco-DB-66-67-PostgreSQL.sql
@@ -6,4 +6,10 @@ UPDATE device SET is_pseudo = false;
 
 UPDATE device SET is_pseudo = true WHERE vendor = 'netdisco';
 
+UPDATE device SET model = 'pseudodevice' WHERE vendor = 'netdisco';
+
+UPDATE device SET os_ver = '2.51.0' WHERE vendor = 'netdisco' AND os_ver IS NULL;
+
+UPDATE device SET os = 'netdisco' WHERE vendor = 'netdisco' AND os IS NULL;
+
 COMMIT;
