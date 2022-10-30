b5e7fa9ab10153821353e67d11c303a169752b07
Oliver Gorwits <ollyg@users.noreply.github.com>
2022-10-30 21:20:46 +0000

    Update PortAccessEntity.pm

--- lib/App/Netdisco/Util/PortAccessEntity.pm
+++ lib/App/Netdisco/Util/PortAccessEntity.pm
@@ -22,37 +22,38 @@ These are updated both during discover and macsuck.
 =cut
 
 sub update_pae_attributes {
-
   my ($device) = @_;
   no warnings "uninitialized";
 
   my $snmp = App::Netdisco::Transport::SNMP->reader_for($device)
     or return Status->defer("pae failed: could not SNMP connect to $device");
-  my $interfaces = $snmp->interfaces;
   debug sprintf ' [%s] pae - updating PortAccessEntity details', $device->ip;
 
   # device property
   my $pae_control = $snmp->pae_control();
 
-  if ($pae_control) {
-    schema('netdisco')->resultset('Device')->search({ 'me.ip' => $device->ip})
-          ->update({ pae_is_enabled => $pae_control eq "enabled" ? 't' : 'f' });
+  if ($pae_control and $pae_control eq 'enabled') {
     debug sprintf ' [%s] pae - PortAccessEntity device-wide support: %s', $device->ip, $pae_control;
+    schema('netdisco')->resultset('Device')->search({ 'me.ip' => $device->ip})
+          ->update({ pae_is_enabled => 't' });
   } else {
     debug sprintf ' [%s] pae - no PortAccessEntity support, leaving worker', $device->ip;
+    schema('netdisco')->resultset('Device')->search({ 'me.ip' => $device->ip})
+          ->update({ pae_is_enabled => 'f' });
     return Status->info("Skipped pae for $device");
   }
 
   # individual port properties
-  my $pae_authconfig_state = $snmp->pae_authconfig_state();
+  my $interfaces = $snmp->interfaces;
+  my $pae_authconfig_state        = $snmp->pae_authconfig_state();
   my $pae_authconfig_port_control = $snmp->dot1xAuthAuthControlledPortControl();
-  my $pae_authconfig_port_status = $snmp->pae_authconfig_port_status();
-  my $pae_authsess_user = $snmp->pae_authsess_user();
-  my $pae_authsess_mab = $snmp->pae_authsess_mab();
-  my $pae_capabilities = $snmp->pae_i_capabilities();
+  my $pae_authconfig_port_status  = $snmp->pae_authconfig_port_status();
+  my $pae_authsess_user           = $snmp->pae_authsess_user();
+  my $pae_authsess_mab            = $snmp->pae_authsess_mab();
+  my $pae_capabilities            = $snmp->pae_i_capabilities();
   my $pae_last_eapol_frame_source = $snmp->pae_i_last_eapol_frame_source();
 
-  for my $ind (sort keys %$interfaces){
+  foreach my $ind (sort keys %$interfaces){
     debug sprintf ' [%s] pae - attributes found for ifindex %s: %s %s %s %s %s %s %s', 
       $device->ip, $ind, 
       $pae_authconfig_state->{$ind} || 'no pae_authconfig_state', 
