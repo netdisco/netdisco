627e425f9164bfb494df52f10caeedfc84b759bd
Oliver Gorwits <oliver@cpan.org>
2022-12-02 16:14:45 +0000

    make display work; relies on T::T calling dict slot or method with same syntax

--- lib/App/Netdisco/Web/CustomFields.pm
+++ lib/App/Netdisco/Web/CustomFields.pm
@@ -6,7 +6,7 @@ use Dancer::Plugin::Auth::Extensible;
 
 use App::Netdisco::Web::Plugin;
 
-set('_inline_actions' => []);
+my @inline_actions = ();
 
 foreach my $config (@{ setting('custom_fields')->{'device'} || [] }) {
 
@@ -20,9 +20,17 @@ foreach my $config (@{ setting('custom_fields')->{'device'} || [] }) {
     label => ($config->{'label'} || ucfirst $config->{'name'}),
   });
 
-  push @{ setting('_inline_actions') },
-       ('device_custom_field_' . $config->{'name'});
+  push @inline_actions, $config->{'name'};
 
 }
+
+schema(vars->{'tenant'})->resultset('Device')->result_source
+  ->resultset_attributes({ '+columns' => {
+    map {( $_ => \[ 'me.custom_fields ->> ?' => $_ ] )}
+        @inline_actions
+  } });
+
+set('_inline_actions' => [ map {'device_custom_field_' . $_}
+                               @inline_actions ]);
 
 true;
