3f8ffe787f3af1b8eacf1e712475b1096c490b2e
Oliver Gorwits <oliver@cpan.org>
2023-04-30 22:59:20 +0100

    hooks only queued if their trigger job is successful (done)

--- lib/App/Netdisco/Worker/Plugin/Macsuck/Hooks.pm
+++ lib/App/Netdisco/Worker/Plugin/Macsuck/Hooks.pm
@@ -11,6 +11,12 @@ register_worker({ phase => 'late' }, sub {
   my ($job, $workerconf) = @_;
   my $count = 0;
 
+  my $best = $job->best_status;
+  if (Status->$best->level != Status->done->level) {
+      return Status
+        ->info(sprintf ' [%s] hooks - skipping due to incomplete job', $job->device);
+  }
+
   foreach my $conf (@{ setting('hooks') }) {
     my $no   = ($conf->{'filter'}->{'no'}   || []);
     my $only = ($conf->{'filter'}->{'only'} || []);
