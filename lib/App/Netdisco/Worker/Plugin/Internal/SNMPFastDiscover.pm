5efcdd77076300feb7de34ad42781adef82a9eee
Oliver Gorwits <oliver@cpan.org>
2023-08-11 13:28:55 +0100

    better status reporting from SNMPFastDiscover

--- lib/App/Netdisco/Worker/Plugin/Internal/SNMPFastDiscover.pm
+++ lib/App/Netdisco/Worker/Plugin/Internal/SNMPFastDiscover.pm
@@ -14,9 +14,10 @@ register_worker({ phase => 'check', driver => 'direct' }, sub {
       and (not blessed $job->device or not $job->device->in_storage)) {
 
       config->{'snmp_try_slow_connect'} = false;
-      debug sprintf '[%s] skipping long SNMP timeouts for initial discover',
-        $job->device;
+      return Status->info("-> running with fast SNMP timeouts for initial discover");
   }
+
+  return Status->info("-> running with configured SNMP timeouts");
 });
 
 true;
