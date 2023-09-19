c651ff107b1ae5eac9ac0bda79f816171a1a1dbf
Oliver Gorwits <ollyg@users.noreply.github.com>
2023-09-19 08:11:05 +0100

    collapse on inventory for long lists (#1103)
    implements #1083

--- lib/App/Netdisco/Web/Plugin/Inventory.pm
+++ lib/App/Netdisco/Web/Plugin/Inventory.pm
@@ -16,19 +16,53 @@ get '/inventory' => require_login sub {
     my $platforms = schema(vars->{'tenant'})->resultset('Device')->get_platforms();
     my $releases = schema(vars->{'tenant'})->resultset('Device')->get_releases();
 
-    my %release_map = (
+    my %release_version_map = (
       map  { (join '', map {sprintf '%05s', $_} split m/(\D)/, ($_->{os_ver} || '')) => $_ }
       $releases->hri->all
     );
-    my @release_list =
-      map  { $release_map{$_} }
-      sort {(($release_map{$a}->{os} || '') cmp ($release_map{$b}->{os} || '')) || ($a cmp $b)}
+
+    my %release_map = ();
+    map  { push @{ $release_map{ $release_version_map{$_}->{os} } }, $release_version_map{$_} }
+    grep { $release_version_map{$_}->{os} }
+    grep { $_ }
+    sort {(lc($release_version_map{$a}->{os} || '') cmp lc($release_version_map{$b}->{os} || '')) || ($a cmp $b)}
+         keys %release_version_map;
+
+    my %release_totals =
+      map  { $_ => {rows => scalar @{ $release_map{$_} }, count => 0} }
+      grep { $_ }
            keys %release_map;
+
+    foreach my $r (keys %release_totals) {
+      map { $release_totals{$r}->{count} += $_->{count} }
+          @{ $release_map{ $r } };
+    }
+
+    my %platform_map = ();
+    map  { push @{ $platform_map{$_->{vendor}} }, $_ }
+    grep { $_->{vendor} }
+    grep { $_ }
+    sort {(lc($a->{vendor} || '') cmp lc($b->{vendor} || '')) || (lc($a->{model} || '') cmp lc($b->{model} || ''))}
+         $platforms->hri->all;
+
+    my %platform_totals =
+      map  { $_ => {rows => scalar @{ $platform_map{$_} }, count => 0} }
+      grep { $_ }
+           keys %platform_map;
+
+    foreach my $r (keys %platform_totals) {
+      map { $platform_totals{$r}->{count} += $_->{count} }
+          @{ $platform_map{ $r } };
+    }
 
     var(nav => 'inventory');
     template 'inventory', {
-      platforms => [ $platforms->hri->all ],
-      releases  => [ @release_list ],
+      platforms => [ sort keys %platform_totals ],
+      releases  => [ sort keys %release_totals ],
+      platform_map => \%platform_map,
+      release_map  => \%release_map,
+      platform_totals => \%platform_totals,
+      release_totals  => \%release_totals,
     }, { layout => 'main' };
 };
 
