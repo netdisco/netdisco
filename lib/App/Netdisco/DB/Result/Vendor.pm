59e1e44cf0016dc38b107ffd36879971d169c1fe
Oliver Gorwits <oliver@cpan.org>
2023-11-13 11:01:49 +0000

    new oui importer using IEEE csv for MA-L+M+S

---
+++ lib/App/Netdisco/DB/Result/Vendor.pm
@@ -0,0 +1,33 @@
+use utf8;
+package App::Netdisco::DB::Result::Vendor;
+
+use strict;
+use warnings;
+
+# CREATE TABLE vendor (company text, abbrev text, base text PRIMARY KEY, bits integer, first macaddr, last macaddr, range int8range, oui varchar(8), EXCLUDE USING GIST (range WITH &&));
+
+use base 'App::Netdisco::DB::Result';
+__PACKAGE__->table("vendor");
+
+__PACKAGE__->add_columns(
+  "company",
+  { data_type => "text", is_nullable => 1 },
+  "abbrev",
+  { data_type => "text", is_nullable => 1 },
+  "base",
+  { data_type => "text", is_nullable => 0 },
+  "bits",
+  { data_type => "integer", is_nullable => 1 },
+  "first",
+  { data_type => "macaddr", is_nullable => 1 },
+  "last",
+  { data_type => "macaddr", is_nullable => 1 },
+  "range",
+  { data_type => "int8range", is_nullable => 1 },
+  "oui",
+  { data_type => "varchar", is_nullable => 1, size => 8 },
+);
+
+__PACKAGE__->set_primary_key("base");
+
+1;
