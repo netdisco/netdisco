5941f865fc39f606ce0d383a19d55f72fad8e4ec
Oliver Gorwits <oliver@cpan.org>
2023-12-07 07:48:35 +0000

    word boundaries on matching

--- lib/App/Netdisco/Web/GenericReport.pm
+++ lib/App/Netdisco/Web/GenericReport.pm
@@ -82,7 +82,7 @@ foreach my $report (@{setting('reports')}) {
       my $rev4 = RE_net_IPv4(-keep);
       my $rev6 = RE_net_IPv6(-keep);
       my $remac = RE_net_MAC(-keep);
-      # my $redom = RE_net_domain(-keep, -nospace);
+      #my $redom = RE_net_domain(-keep, -nospace, -rfc1101);
 
       foreach my $row (@results) {
           foreach my $col (@column_order) {
@@ -90,26 +90,26 @@ foreach my $report (@{setting('reports')}) {
               my $fields = (ref $row->{$col} ? $row->{$col} : [$row->{$col}]);
 
               foreach my $f (@$fields) {
-                  # seems far too sensitive match :-(
-                  #$f =~ s!${redom}!'<a href="'.
+                  # seems too sensitive match to be useful :-(
+                  #$f =~ s!\b${redom}\b!'<a href="'.
                   #  uri_for('/search', {q => $1 .($2 ? "/$2" : '')})->path_query
                   #  .'">'. encode_entities($1 .($2 ? "/$2" : '')) .'</a>'!gex;
 
-                  $f =~ s!${recidr4}!'<a href="'.
+                  $f =~ s!\b${recidr4}\b!'<a href="'.
                     uri_for('/search', {q => "$1/$2"})->path_query
                     .'">'. encode_entities("$1/$2") .'</a>'!gex;
 
                   if (not $1 and not $2) {
-                      $f =~ s!${rev4}!'<a href="'.
+                      $f =~ s!\b${rev4}\b!'<a href="'.
                         uri_for('/search', {q => $1})->path_query
                         .'">'. encode_entities($1) .'</a>'!gex;
                   }
 
-                  $f =~ s!${rev6}!'<a href="'.
+                  $f =~ s!\b${rev6}\b!'<a href="'.
                     uri_for('/search', {q => $1})->path_query
                     .'">'. encode_entities($1) .'</a>'!gex;
 
-                  $f =~ s!${remac}!'<a href="'.
+                  $f =~ s!\b${remac}\b!'<a href="'.
                     uri_for('/search', {q => $1})->path_query
                     .'">'. encode_entities($1) .'</a>'!gex;
 
