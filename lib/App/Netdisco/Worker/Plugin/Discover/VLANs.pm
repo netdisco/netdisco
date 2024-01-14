1e977641578381995207c8f30b997586ab40f575
Oliver Gorwits <oliver@cpan.org>
2024-01-14 16:38:28 +0000

    set device ports with multi vlans to be uplinks

--- lib/App/Netdisco/Worker/Plugin/Discover/VLANs.pm
+++ lib/App/Netdisco/Worker/Plugin/Discover/VLANs.pm
@@ -86,6 +86,12 @@ register_worker({ phase => 'main', driver => 'snmp' }, sub {
       }
   }
 
+  # set is_uplink on the ports with non-native vlans
+  foreach my $pv (@portvlans) {
+      next unless $pv->{native} and $pv->{native} eq 'f';
+      $device_ports->{$pv->{port}}->update({is_uplink => \'true'});
+  }
+
   schema('netdisco')->txn_do(sub {
     my $gone = $device->port_vlans->delete;
     debug sprintf ' [%s] vlans - removed %d port VLANs',
