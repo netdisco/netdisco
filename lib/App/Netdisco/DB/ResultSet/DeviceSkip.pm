beffcb85592a07bb31050793ef5d9a8d9fac12ed
Oliver Gorwits <oliver@cpan.org>
2024-04-12 14:07:29 +0100

    #1199 wrap find_or_create in EXCLUSIVE table lock transactions

---
+++ lib/App/Netdisco/DB/ResultSet/DeviceSkip.pm
@@ -0,0 +1,11 @@
+package App::Netdisco::DB::ResultSet::DeviceSkip;
+use base 'App::Netdisco::DB::ResultSet';
+
+use strict;
+use warnings;
+
+__PACKAGE__->load_components(qw/
+  +App::Netdisco::DB::ExplicitLocking
+/);
+
+1;
