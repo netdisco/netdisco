3a1e7dda0c3144e7f9791ee325a8f11952c94d6c
Oliver Gorwits <oliver@cpan.org>
2024-07-25 16:36:34 +0100

    change Job class to be a frozen @dataclass

--- share/python/netdisco/netdisco/util/configuration.py
+++ share/python/netdisco/netdisco/util/configuration.py
@@ -6,8 +6,16 @@ This module provides a utility function setting() to get one of Netdisco's
 configuration settings.
 """
 
+import logging
+from os import getpid
+
 import os
 import json
+from dataclasses import dataclass, InitVar
+
+# avoid circular dependency, this module may need to log.debug itself
+logging.basicConfig(format=('[' + str(getpid()) + '] %(message)s'), level=getattr(logging, 'DEBUG'))
+log = logging.getLogger(__name__)
 
 ND2_JOB_CONFIGURATION = json.loads(os.environ['ND2_JOB_CONFIGURATION'])
 ND2_RUNTIME_CONFIGURATION = json.loads(os.environ['ND2_RUNTIME_CONFIGURATION'])
@@ -18,10 +26,33 @@ def setting(name):
     return ND2_RUNTIME_CONFIGURATION[name]
 
 
+@dataclass(frozen=True)
 class Job:
-    # TODO make readonly attributes
-    def __init__(self, iterable=(), **kwargs):
-        self.__dict__.update(iterable, **kwargs)
+    job: int
+    action: str
+    entered: str = ''
+    started: str = ''
+    finished: str = ''
+    device: str = ''
+    port: str = ''
+    only_namespace: str = ''
+    subaction: str = ''
+    status: str = ''
+    username: str = ''
+    userip: str = ''
+    log: str = ''
+    device_key: str = ''
+    job_priority: str = ''
+    is_cancelled: bool = False
+    is_offline: bool = False
+
+    _current_phase: InitVar[str] = ''
+    _last_namespace: InitVar[str] = ''
+    _last_priority: InitVar[int] = 0
+    _statuslist: InitVar[list] = []  # field(default_factory=list)
+
+    def id(self) -> int:
+        return self.job
 
 
-job = Job(ND2_JOB_CONFIGURATION)
+job = Job(**ND2_JOB_CONFIGURATION)
