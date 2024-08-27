df56ed25bf65a5f74a552f82642145d867696db1
Oliver Gorwits <oliver@cpan.org>
2024-08-27 07:42:34 +0100

    move to using bgp summary for route neighbors

--- share/python/netdisco/netdisco/worklet/discover/nexthopneighbors/main/cli/juniper_junos.py
+++ share/python/netdisco/netdisco/worklet/discover/nexthopneighbors/main/cli/juniper_junos.py
@@ -2,19 +2,19 @@ from netdisco.util.worklet import debug, context as c
 from netdisco.util.ssh import net_connect
 
 def main():
-    command = "show route forwarding-table | match ucst | match /32"
+    command = "show bgp summary"
     with net_connect:
         output = net_connect.send_command(command, use_textfsm=True)
 
     neighbors = []
     if output is not None:
-        # debug(output)
+        debug(output)
         for record in output:
-            neighbors.append(record['next_hop'])
+            neighbors.append(record['peer_ip'])
 
-    # debug(neighbors)
+    debug(neighbors)
     c.stash.set('next_hops', neighbors)
-    c.status.info('gathered route neighbors')
+    c.status.info('gathered bgp next hops')
 
 
 if __name__ == '__main__':
