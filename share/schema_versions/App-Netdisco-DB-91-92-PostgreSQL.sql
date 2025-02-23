f9d51e521928751bed9256658ad2cbfe3de58862
Oliver Gorwits <oliver@cpan.org>
2025-02-23 20:59:08 +0000

    drop leaf and munge cols from device_browser

--- share/schema_versions/App-Netdisco-DB-91-92-PostgreSQL.sql
+++ share/schema_versions/App-Netdisco-DB-91-92-PostgreSQL.sql
@@ -1,5 +1,11 @@
 BEGIN;
 
+DROP INDEX IF EXISTS idx_device_browser_ip_leaf;
+
+ALTER TABLE device_browser DROP COLUMN leaf;
+
+ALTER TABLE device_browser DROP COLUMN munge;
+
 CREATE TABLE snmp_filter (
     "leaf"    "text" NOT NULL,
     "subname"    "text" NOT NULL,
