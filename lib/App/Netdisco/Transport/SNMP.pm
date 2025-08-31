90d47ac14226ee0b24496178aa914287b8c575d5
Oliver Gorwits <oliver@cpan.org>
2025-08-31 18:42:23 +0100

    avoid having to always try snmp v3 when known good v2 tag

--- lib/App/Netdisco/Transport/SNMP.pm
+++ lib/App/Netdisco/Transport/SNMP.pm
@@ -200,7 +200,36 @@ sub _snmp_connect_generic {
       unshift @classes, $device->snmp_class;
   }
 
-  # first try the communities in a fast pass using best version
+  # try last known-good by tag if it's stored
+  # this gets in the way of SNMP version upgrade (2 to 3)
+  # but can use only/no to get around that
+
+  my $tag_name = 'snmp_auth_tag_'. $mode;
+  my $stored_tag = eval { $device->community->$tag_name };
+
+  if ($device->in_storage and $stored_tag) {
+      debug sprintf '[%s:%s] try_connect with cached tag %s',
+          $snmp_args{DestHost}, $snmp_args{RemotePort}, $stored_tag;
+
+      my $comm = $communities[0];
+      my $ver = (exists $comm->{community} ? 2 : 3);
+      my %local_args = (%snmp_args,
+        Version => $ver, Retries => 0, Timeout => 200000);
+        
+      my $info = _try_connect($device, $classes[0], $comm, $mode, \%local_args,
+            ($useclass ? 0 : 1) );
+      # if successful, restore the default/user timeouts and return
+      if ($info) {
+          my $class = ($useclass ? $classes[0] : $info->device_type);
+          return $class->new(
+            %snmp_args, Version => $ver,
+            ($info->offline ? (Cache => $info->cache) : ()),
+            _mk_info_commargs($comm),
+          );
+      }
+  }
+
+  # try the communities in a fast pass using best version
 
   VERSION: foreach my $ver (3, 2) {
       my %local_args = (%snmp_args,
