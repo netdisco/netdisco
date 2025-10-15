7e741c781d12445a16a352999420f3bc41ab4ff0
Oliver Gorwits <oliver@cpan.org>
2025-10-15 12:43:53 +0100

    smarter schema update to only apply db roles to users with a portctl role

--- share/schema_versions/App-Netdisco-DB-94-95-PostgreSQL.sql
+++ share/schema_versions/App-Netdisco-DB-94-95-PostgreSQL.sql
@@ -2,4 +2,6 @@ BEGIN;
 
 ALTER TABLE users ADD COLUMN portctl_checkpoint integer NOT NULL DEFAULT 0;
 
+UPDATE users SET portctl_checkpoint = 1 WHERE portctl_role IS NOT NULL AND portctl_role != '';
+
 COMMIT;
