94af6587788af4feecf80d51360d0bedd676c515
Oliver Gorwits <oliver@cpan.org>
2025-10-24 16:30:16 +0100

    fix typo in schema update file

--- share/schema_versions/App-Netdisco-DB-94-95-PostgreSQL.sql
+++ share/schema_versions/App-Netdisco-DB-94-95-PostgreSQL.sql
@@ -4,6 +4,6 @@ ALTER TABLE node_ip ADD COLUMN vrf text DEFAULT '' NOT NULL;
 
 ALTER TABLE node_ip DROP CONSTRAINT node_ip_pkey;
 
-ALTER TABLE node_ip ADD CONSTRAINT node_ip_pkey PRIMARY KEY (mac, ip, vrf)
+ALTER TABLE node_ip ADD CONSTRAINT node_ip_pkey PRIMARY KEY (mac, ip, vrf);
 
 COMMIT;
