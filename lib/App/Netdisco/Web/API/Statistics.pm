12429cfb6ec0c00d5cd824361e933e3be6903d0e
Oliver Gorwits <oliver@cpan.org>
2026-04-16 10:58:14 +0100

    move statistics api to General

--- lib/App/Netdisco/Web/API/Statistics.pm
+++ lib/App/Netdisco/Web/API/Statistics.pm
@@ -8,15 +8,18 @@ use Dancer::Plugin::Auth::Extensible;
 use Try::Tiny;
 
 swagger_path {
-  tags => ['Statistics'],
-  path => (setting('api_base') || '').'/object/statistics',
+  tags => ['General'],
+  path => (setting('api_base') || '').'/statistics',
   description => 'Returns the latest row from the statistics table',
+  parameters => [],
   responses => { default => {} },
-}, get '/api/v1/object/statistics' => require_role api => sub {
+
+}, get '/api/v1/statistics' => require_role api => sub {
   my $stats = try {
     schema(vars->{'tenant'})->resultset('Statistics')
       ->search(undef, { order_by => { -desc => 'day' }, rows => 1 })->first
   } or send_error('No statistics available', 404);
+
   return to_json $stats->TO_JSON;
 };
 
